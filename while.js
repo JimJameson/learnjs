ca68cd099df9d98755102708d272e9960f049cd2
JimJameson <deejames@mail.ru>
2020-01-21 18:49:59 +0300

    Update while.js

--- while.js
+++ while.js
@@ -18,18 +18,3 @@ for (let i = 1; i < 8; i++) {
     console.log(i);
 }
 
-let x = 5; 
-console.log(++x);
-
-console.log(null || undefined || 0);
-
-let y = 1; 
-let x = y = 2; 
-console.log(2 && 3);
-
-console.log(+"Infinity");
-
-a = []; 
-b = [];
-console.log([ ] + false - null + true);
-console.
